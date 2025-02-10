be5dd4c8af4a1674252ef117b40b54eb2c249657
DARKXSIDE78 <nk1010057@gmail.com>
2025-02-10 09:20:56 +0530

    Update _config.php

--- _config.php
+++ _config.php
@@ -20,9 +20,9 @@ $twitter = "https://x.com/PacaHat";
 
 // all the api you need
 
-$api = "your-hosted-api.com/api/v2/hianime"; //https://github.com/ghoshRitesh12/aniwatch-api Also use this in src/component/qtip.php
-$zpi = "your-hosted-api.com/api"; //https://github.com/itzzzme/anime-api
-$proxy = "your-hosted-proxy.com/cors?url="; //https://github.com/shashstormer/m3u8_proxy-cors
+$api = "https://aniwatch-api-two.vercel.app/api/v2/hianime"; //https://github.com/ghoshRitesh12/aniwatch-api Also use this in src/component/qtip.php
+$zpi = "https://zen-api-phi.vercel.app/api"; //https://github.com/itzzzme/anime-api
+$proxy = "https://m3u8-proxy-cors-three-kohl.vercel.app/cors?url="; //https://github.com/shashstormer/m3u8_proxy-cors
 
 
 $banner = $websiteUrl . "/public/images/banner.png";
